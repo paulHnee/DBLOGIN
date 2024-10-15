69340d9db18e8c14034a1397775ea9e2b6836771
pbuchwald <paul.buchwald@hnee.de>
2024-10-15 10:38:59 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    protected int DB_CHECK(String username, String password) {
+       int check = 0; // ALles sauber
+        if(!username.equals("admin")){
+            return check = 1;
+        }
+        if(!password.equals("admin")){
+            return check = 2;
+        }
+        return check;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter username: ");
+        String username_in = sc.nextLine();
+        System.out.print("Enter password: ");
+        String password_in = sc.nextLine();
+
+
+        if (new Main().DB_CHECK(username_in, password_in) == 1){
+            System.out.println("Username is wrong");
+        } else if (new Main().DB_CHECK(username_in, password_in) == 2) {
+            System.out.println("password is wrong");
+        }
+        else {
+            System.out.println("Willkommen");
+        }
+
+    }
+}
